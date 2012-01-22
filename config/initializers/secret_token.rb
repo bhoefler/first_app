d935c640f97440b944a43674401cd0b501be3840
ikom <ikom@ikom-VirtualBox.(none)>
2012-01-22 15:59:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '8afdcb3dc4b47c0eee9d59077fb9b7ab4dfc2d734e5c1c0fbbdd59cd375320328d6ae9a88886396bd8525971192c67bd066067bf4fc50788ee056449efc8d5ae'
